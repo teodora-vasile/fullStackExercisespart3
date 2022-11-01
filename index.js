7e8809f306cba533b79c0ee92c0e6576c5116640
teodora-vasile <107516538+teodora-vasile@users.noreply.github.com>
2022-11-01 18:07:28 +0200

    Exercise 3.3

--- index.js
+++ index.js
@@ -28,6 +28,18 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if (person){ 
+       response.json(person) 
+       }
+        else { 
+             response.status(404).end() 
+             }
+  })
+
 app.get('/api/info', (request, response) => {
   let personsNumber = `Phonebook has info for ${persons.length} people. <br> ${new Date()}`
   
