13f6d2bd9d637e1959316a12ffe08be46950a6f1
teodora-vasile <107516538+teodora-vasile@users.noreply.github.com>
2022-11-06 21:05:55 +0200

    Exercise 3.12

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 The link to the app is https://phonebookapp1.fly.dev/
+=======
+Exercise 3.12 with mongo
+>>>>>>> Stashed changes
---
+++ mongo.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://phonebook1:${password}@cluster0.z9wt3qc.mongodb.net/phonebookapp1?retryWrites=true&w=majority`
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length > 3){
+mongoose
+  .connect(url)
+  .then((result) => {
+    const person = new Person({
+    name: name,
+    number: number
+     })
+    return person.save()   
+    })
+    .then(() => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    return mongoose.connection.close()
+         })             
+    .catch((err) => console.log(err))  
+        } 
+
+  else {
+  mongoose
+  .connect(url)
+  .then(() => {
+    console.log ('Phonebook:')
+   Person
+  .find({})
+  .then((result) => {
+    result.forEach(person => {
+      console.log(person.name, person.number)
+    })
+    return mongoose.connection.close()
+  })
+})  
+  .catch((err) => console.log(err))  
+}    
